3d0a66a448f6c0291457265e8861cca907cc0ecb
IgorPR0 <igortecnologia2020@gmail.com>
2020-11-20 14:34:15 -0300

    Concluido Tabuada

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.Write($"Tabuada do Numero Sete ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($" Digite 7\n");
+                Console.ReadLine().ToUpper();
+                Console.ResetColor();
+            
+               
+               Console.Write($"7 X 0 = 0\n");
+               Console.Write("7 X 1 = 7\n");
+               Console.Write("7 X 2 = 14\n");
+               Console.Write("7 X 3 = 21\n");
+               Console.Write("7 X 4 = 28\n");
+               Console.Write("7 X 5 = 35\n");
+               Console.Write("7 X 6 = 42\n");
+               Console.Write("7 X 7 = 49\n");
+               Console.Write("7 X 8 = 56\n");
+               Console.Write("7 X 9 = 63\n");
+               Console.Write("7 X 10 = 70\n");
+
+
+        }
+    }
+}
